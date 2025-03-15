1e7bea85b43ac1132c6f850eebbd620c7c400c12
EpicnessTwo <github@epickitty.uk>
2025-03-15 15:18:57 +0000

    Added user sessions, version and dereferencing to the Ras Service

--- app/Services/RasService.php
+++ app/Services/RasService.php
@@ -5,17 +5,21 @@ namespace App\Services;
 use EpicKitty\Ras\Api\DefaultApi;
 use EpicKitty\Ras\ApiException;
 use EpicKitty\Ras\Configuration;
+use EpicKitty\Ras\Model\ChatRoomPrivateGet200ResponseInner;
+use EpicKitty\Ras\Model\SessionGet200Response;
 use EpicKitty\Ras\Model\UserDeleteRequest;
 use EpicKitty\Ras\Model\UserPasswordPutRequest;
 use EpicKitty\Ras\Model\UserPostRequest;
 use EpicKitty\Ras\Model\UserScreennameAccountGet200Response;
+use EpicKitty\Ras\Model\VersionGet200Response;
 
 class RasService
 {
     public $ras;
 
-    public function __construct()
-    {
+    public function __construct(
+        public bool $dereference = false
+    ) {
         $config = new Configuration();
         $config->setHost(config('ras.api_uri'));
         $this->ras = new DefaultApi(
@@ -80,16 +84,40 @@ class RasService
     /**
      * @throws ApiException
      */
-    public function getPrivateChatRooms(): array
+    public function getUserSessions(string $screenName): SessionGet200Response|array|null
     {
-        return $this->ras->chatRoomPrivateGet();
+        return $this->processResponse($this->ras->sessionScreennameGet($screenName));
     }
 
     /**
      * @throws ApiException
      */
-    public function getPublicChatRooms(): array
+    public function getPrivateChatRooms(): ChatRoomPrivateGet200ResponseInner|array|null
     {
-        return $this->ras->chatRoomPublicGet();
+        return $this->processResponse($this->ras->chatRoomPrivateGet());
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function getPublicChatRooms(): ChatRoomPrivateGet200ResponseInner|array|null
+    {
+        return $this->processResponse($this->ras->chatRoomPublicGet());
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function getVersion(): VersionGet200Response|array|null
+    {
+        return $this->processResponse($this->ras->versionGet());
+    }
+
+    private function processResponse($response)
+    {
+        if ($this->dereference) {
+            return json_decode(json_encode($response), true);
+        }
+        return $response;
     }
 }
