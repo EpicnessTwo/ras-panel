83bcdfd952eb56edb2c6fd24100f158c112f5437
EpicnessTwo <github@epickitty.uk>
2025-03-16 23:52:38 +0000

    Does help if I change it in both places!

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -111,7 +111,7 @@ class DashboardController extends Controller
         // Generate a random ICQ number that doesn't already exist
         $icq = rand(100000, 999999);
         while (Icq::whereName($icq)->exists()) {
-            $icq = rand(100000000, 999999999);
+            $icq = rand(100000, 999999);
         }
 
         $ras = new RasService();
