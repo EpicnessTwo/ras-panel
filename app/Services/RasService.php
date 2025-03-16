9afddce9a02eb9773ab23e7410b582e2343ad96f
EpicnessTwo <github@epickitty.uk>
2025-03-16 22:21:45 +0000

    Added new functionality to RasService

--- app/Services/RasService.php
+++ app/Services/RasService.php
@@ -6,6 +6,9 @@ use EpicKitty\Ras\Api\DefaultApi;
 use EpicKitty\Ras\ApiException;
 use EpicKitty\Ras\Configuration;
 use EpicKitty\Ras\Model\ChatRoomPrivateGet200ResponseInner;
+use EpicKitty\Ras\Model\ChatRoomPublicPostRequest;
+use EpicKitty\Ras\Model\DirectoryCategoryPostRequest;
+use EpicKitty\Ras\Model\DirectoryKeywordPostRequest;
 use EpicKitty\Ras\Model\SessionGet200Response;
 use EpicKitty\Ras\Model\UserDeleteRequest;
 use EpicKitty\Ras\Model\UserPasswordPutRequest;
@@ -32,7 +35,7 @@ class RasService
      */
     public function getUsers(): array
     {
-        return $this->ras->userGet();
+        return $this->processResponse($this->ras->userGet());
     }
 
     /**
@@ -40,7 +43,7 @@ class RasService
      */
     public function getUser(string $screenName): UserScreennameAccountGet200Response
     {
-        return $this->ras->userScreennameAccountGet($screenName);
+        return $this->processResponse($this->ras->userScreennameAccountGet($screenName));
     }
 
     /**
@@ -84,8 +87,18 @@ class RasService
     /**
      * @throws ApiException
      */
-    public function getUserSessions(string $screenName): SessionGet200Response|array|null
+    public function getSessions(): SessionGet200Response|array|null
     {
+        return $this->processResponse($this->ras->sessionGet());
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function getUserSessions(string $screenName = null): SessionGet200Response|array|null
+    {
+        $screenName = $screenName ?? auth()->user()->name;
+
         return $this->processResponse($this->ras->sessionScreennameGet($screenName));
     }
 
@@ -96,7 +109,6 @@ class RasService
     {
         return $this->processResponse($this->ras->chatRoomPrivateGet());
     }
-
     /**
      * @throws ApiException
      */
@@ -108,11 +120,75 @@ class RasService
     /**
      * @throws ApiException
      */
+    public function createPublicChatRoom(string $name): void
+    {
+        $this->ras->chatRoomPublicPost(
+            new ChatRoomPublicPostRequest([
+                    'name' => $name
+            ])
+        );
+    }
+
+    public function getCategories(): array
+    {
+        return $this->processResponse($this->ras->directoryCategoryGet());
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function createCategory(string $name): void
+    {
+        $this->ras->directoryCategoryPost(
+            new DirectoryCategoryPostRequest([
+                'name' => $name
+            ])
+        );
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function deleteCategory(int $id): void
+    {
+        $this->ras->directoryCategoryIdDelete($id);
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function getCategoryKeywords(int $id): array
+    {
+        return $this->processResponse($this->ras->directoryCategoryIdKeywordGet($id));
+    }
+
+    /**
+     * @throws ApiException
+     */
+    public function addCategoryKeyword(int $id, string $name): void
+    {
+        $this->ras->directoryKeywordPost(
+            new DirectoryKeywordPostRequest([
+                'id' => $id,
+                'name' => $name
+            ])
+        );
+    }
+
+    /**
+     * @throws ApiException
+     */
     public function getVersion(): VersionGet200Response|array|null
     {
         return $this->processResponse($this->ras->versionGet());
     }
 
+    /**
+     * Objects are often messy, so we can convert them to arrays.
+     *
+     * @param mixed $response
+     * @return mixed
+     */
     private function processResponse($response)
     {
         if ($this->dereference) {
