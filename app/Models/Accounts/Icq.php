38c960e78d35f18f253dff49c4d209a7afa8d1b5
EpicnessTwo <github@epickitty.uk>
2025-03-16 22:20:43 +0000

    Added new models for ICQ and AIM

---
+++ app/Models/Accounts/Icq.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Icq extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
